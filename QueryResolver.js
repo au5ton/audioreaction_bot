bbaae984669b38d801bf29e3e00ee9de0b636253
Austin Jackson <austinjckson@gmail.com>
2018-01-24 13:18:02 -0600

    i'm dumb

--- QueryResolver.js
+++ QueryResolver.js
@@ -8,8 +8,8 @@ _.search = (query) => {
     return new Promise((resolve, reject) => {
 		let results = [];
         for(let i in ReactionLoader.getLiveCache()) {
-            if(Reactions[i].title.toLowerCase().includes(query.toLowerCase())) {
-                results.push(Reactions[i]);
+            if(ReactionLoader.getLiveCache()[i].title.toLowerCase().includes(query.toLowerCase())) {
+                results.push(ReactionLoader.getLiveCache()[i]);
             }
         }
         resolve(results.slice(0,20)) //this is safe because Array.slice() can't go out of bounds (even with negative numbers)
